68aaa91c6070ea3a639f387a2c69d07af320bf5b
Angel Mike <angelmikecatambcan1@gmail.com>
2025-02-01 20:15:28 +0700

    Login Panel

--- login_handler.php
+++ login_handler.php
@@ -1,8 +1,8 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
 
     // Add your authentication logic here
     // Example: Replace this with your actual validation code
